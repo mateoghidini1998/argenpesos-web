d493681dcb00f0a98182fd1ae41261e4e9aa1b4c
mateoghidini1998 <mateghidini123@gmail.com>
2024-10-08 22:20:24 -0300

    Fix header and main banner spacing

--- src/app/components/MainBanner.tsx
+++ src/app/components/MainBanner.tsx
@@ -18,7 +18,7 @@ export default function MainBanner() {
 
     return (
         <Dialog open={isChatOpen} onOpenChange={setIsChatOpen}>
-            <div className={`mt-[80px] md:mt-[100px] lg:mt-[132px]`}>
+            <div className={`mt-[105px] md:mt-[100px] lg:mt-[132px]`}>
                 <section className="flex relative w-full h-[250px] md:h-[400px] lg:h-[550px] bg-white hero-background mb-[75px] rounded-b-[30px] sm:rounded-b-[50px] md:rounded-b-[70px] lg:rounded-b-[100px]">
                     <div className='w-full'>
                         <div className="w-1/2 flex flex-col justify-center items-center h-[250px] md:h-[400px] lg:h-[550px] px-2 sm:px-8 md:px-12 lgx:p-14 xl:px-20">
