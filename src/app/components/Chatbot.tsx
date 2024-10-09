a69198f20acecb46fbe13cf866d9db5e9adb79b1
mateoghidini1998 <mateghidini123@gmail.com>
2024-10-08 21:17:07 -0300

    add security protocl

--- src/app/components/Chatbot.tsx
+++ src/app/components/Chatbot.tsx
@@ -119,7 +119,7 @@ export default function Chatbot() {
           const dni = userData.dni;
           try {
             const response = await fetch(
-              `http://smarter.argenpesos.com.ar:30002/External/validacionidentidad?ticket=EB56789C-B3B9-4D4A-A6C8-98235B1179C8&documento=${dni}&sexo=${sexoNumerico}`
+              `https://smarter.argenpesos.com.ar:30002/External/validacionidentidad?ticket=EB56789C-B3B9-4D4A-A6C8-98235B1179C8&documento=${dni}&sexo=${sexoNumerico}`
             );
             const data = await response.json();
 
