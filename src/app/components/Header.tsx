11b98352d10059314b41926cceeaace2c5ab3ee4
mateoghidini1998 <mateghidini123@gmail.com>
2024-11-12 15:57:24 -0300

    Adjust logo size

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
         <header className={`w-full max-w-screen ${isNavbarOpen ? 'h-screen' : 'h-auto'} lg:h-[152px] gap-8  flex items-center justify-apart xl:justify-between py-6 px-4 lg:px-[40px] 2xl:px-[60px] border-b-[1px] border-b-[#bebebe] fixed top-0 left-0 bg-white z-50`}>
             <div className={`hidden lg:block  `}>
             <Link href="/" passHref>
-                <img src="./logo.png" alt="argenpesos" className="md:w-[150px] md:h-[100px] lg:w-[110px] lg:h-[70px] 2xl:h-[90px] 2xl:w-[220px]" />
+                <img src="./logo.png" alt="argenpesos" className="md:w-[150px] md:h-[100px] lg:w-[110px] lg:h-[70px] xl:h-[90px] xl:w-[220px]" />
             </Link>
             </div>
             <Navbar onNavbarToggle={handleNavbarToggle} />
