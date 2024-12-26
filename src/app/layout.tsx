d690259061ea4c92a44b085d40c2f9bd6814c50b
mateoghidini1998 <50156851+mateoghidini1998@users.noreply.github.com>
2024-12-26 16:19:02 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import FavIcon from "../../public/favicon-argenpesos.svg"
 
 
 export const metadata: Metadata = {
@@ -17,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="gotham">
+      <head>
+        <link rel="icon" href={FavIcon} />
+      </head>
       <body>
         <Header/>
         {children}
