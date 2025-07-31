cde366829d15054e1e82bacb56f37b75f2a1a662
mateoghidini1998 <mateghidini123@gmail.com>
2025-07-31 13:12:47 -0300

    Remove console log for published products in ArgenCompras API route to clean up code and improve performance.

--- src/app/api/argencompras/route.ts
+++ src/app/api/argencompras/route.ts
@@ -20,7 +20,6 @@ export async function GET(req: NextRequest) {
         
         const data = await response.json();
         const publishedProducts = data.filter((product: any) => product.published === true);
-        console.log(publishedProducts);
     
         return NextResponse.json(publishedProducts);
     } catch (error) {
