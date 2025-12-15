54277fb54b53b2c7438d1c03954c470aad675bc0
mateoghidini1998 <mateghidini123@gmail.com>
2025-12-15 12:45:30 -0300

    Add navidad logo SVG and update Header component to use new logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
         <header className={`w-full max-w-screen ${isNavbarOpen ? 'h-screen' : 'h-auto'} lg:h-[152px] gap-8  flex items-center justify-between  py-6 px-4 lg:px-[40px] 2xl:px-[60px] border-b-[1px] border-b-[#bebebe] fixed top-0 left-0 bg-white z-50`}>
             <div className={`${isNavbarOpen ? 'hidden' : 'block'}`}>
                 <Link href="/" passHref>
-                    <img src="./logo.png" alt="argenpesos" className="md:w-[150px] md:h-[100px] lg:w-[110px] lg:h-[70px] xl:h-[60px] xl:w-[190px]" />
+                    <img src="./navidad-logo.svg" alt="argenpesos" className="md:w-[150px] md:h-[100px] lg:w-[110px] lg:h-[80px] xl:h-[100px] xl:w-[190px]" />
                 </Link>
             </div>
             <Navbar onNavbarToggle={handleNavbarToggle} />
