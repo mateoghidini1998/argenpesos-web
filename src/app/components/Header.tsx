1b1b2c0bb149507a9b07109a70d363461a82348f
mateoghidini1998 <mateghidini123@gmail.com>
2026-01-09 23:25:59 -0300

    Refactor Header component to improve code readability and update logo image source

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,26 +1,31 @@
-'use client'
+"use client";
 import { useState } from "react";
 import Navbar from "./Navbar";
 import Link from "next/link";
 
 export default function Header() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
-    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const handleNavbarToggle = (isOpen) => {
+    setIsNavbarOpen(isOpen);
+  };
 
-    const handleNavbarToggle = (isOpen) => {
-        setIsNavbarOpen(isOpen);
-    };
-
-
-    return (
-        <header className={`w-full max-w-screen ${isNavbarOpen ? 'h-screen' : 'h-auto'} lg:h-[152px] gap-8  flex items-center justify-between  py-6 px-4 lg:px-[40px] 2xl:px-[60px] border-b-[1px] border-b-[#bebebe] fixed top-0 left-0 bg-white z-50`}>
-            <div className={`${isNavbarOpen ? 'hidden' : 'block'}`}>
-                <Link href="/" passHref>
-                    <img src="./navidad-logo.svg" alt="argenpesos" className="md:w-[150px] md:h-[100px] lg:w-[110px] lg:h-[80px] xl:h-[100px] xl:w-[190px]" />
-                </Link>
-            </div>
-            <Navbar onNavbarToggle={handleNavbarToggle} />
-        </header>
-    );
-    
+  return (
+    <header
+      className={`w-full max-w-screen ${
+        isNavbarOpen ? "h-screen" : "h-auto"
+      } lg:h-[152px] gap-8  flex items-center justify-between  py-6 px-4 lg:px-[40px] 2xl:px-[60px] border-b-[1px] border-b-[#bebebe] fixed top-0 left-0 bg-white z-50`}
+    >
+      <div className={`${isNavbarOpen ? "hidden" : "block"}`}>
+        <Link href="/" passHref>
+          <img
+            src="./logo.png"
+            alt="argenpesos"
+            className="md:w-[150px] md:h-[100px] lg:w-[110px] lg:h-[70px] xl:h-[60px] xl:w-[190px]"
+          />{" "}
+        </Link>
+      </div>
+      <Navbar onNavbarToggle={handleNavbarToggle} />
+    </header>
+  );
 }
